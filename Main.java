b9f89420c1d04f22830fbec1b075fdb45ef944d1
soop <110252@eesd.org>
2021-03-22 22:00:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int rawblocks = 69420;
+    System.out.println("this code sucks");
+    if (rawblocks == 69420);
+    System.out.println("welcome to discount thing");
+    rawblocks += 69420;
+    if(rawblocks == 138840);
+    System.out.println("your too cool");
+    if(rawblocks != 138840);
+    System.out.println("your bad at video games");
+  }
+}
