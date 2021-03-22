d9830ba3764ff31626e1fe3a31760f41535da156
Do Bryan <110252@eesd.org>
2021-03-22 22:07:16 +0000

    indents so it's much more "cleaner"

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ class Main {
   public static void main(String[] args) {
     int rawblocks = 69420;
     System.out.println("this code sucks");
-    if (rawblocks == 69420);
-    System.out.println("welcome to discount thing");
-    rawblocks += 69420;
+    if(rawblocks == 69420);
+      System.out.println("welcome to discount thing");
+      rawblocks += 69420;
     if(rawblocks == 138840);
-    System.out.println("your too cool");
+      System.out.println("your too cool");
     if(rawblocks != 138840);
-    System.out.println("your bad at video games");
+      System.out.println("your bad at video games");
   }
 }
